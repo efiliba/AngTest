a9dbbdeb654361c3112314b9ad66f728aa4b7532
Eli <efiliba@hotmail.com>
2017-02-26 15:56:11 +1100

    Clean up Timer Component

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -6,26 +6,45 @@ import { Observable } from 'rxjs/Observable';
     <h1>Timer</h1>
     <button id='start'>Start</button>
     <button #stop>Stop</button>
-    <button (click)='onResetClicked($event)'>Reset</button>
+    <!--<button (click)='onResetClicked($event)'>Reset</button>     // Replace events  -->
+    <button #reset>Reset</button>
+    <div #timer></div>
   `
 })
 export class TimerComponent implements OnInit {
-  @ViewChild('stop') stopButton;
-
-  constructor() { }
+  @ViewChild('stop') stopButton: ElementRef;
+  @ViewChild('reset') resetButton: ElementRef;
+  @ViewChild('timer') timer: ElementRef;
 
   ngOnInit() {
     const startButton = document.querySelector('#start');
 
-    Observable.fromEvent(startButton, 'click')
-      .subscribe(() => console.log('start clicked'));
+    const start$: Observable<any> = Observable.fromEvent(startButton, 'click');
+    const stop$ = Observable.fromEvent(this.stopButton.nativeElement, 'click');
+    const reset$ = Observable.fromEvent(this.resetButton.nativeElement, 'click');
+    const interval$ = Observable.interval(100);
 
-    Observable.fromEvent(this.stopButton.nativeElement, 'click')
-      .subscribe(() => console.log('stop clicked'));
+    const intervalThatStops$ = interval$
+      .takeUntil(stop$);
+
+    const startInterval$ = start$
+      .switchMapTo(intervalThatStops$);                 // Timer only starts when Start clicked
+    
+    const initialState: number = 0;
+    const increment: (acc: number) => number = acc => acc + 1;
+    const reset: (x: void) => number = () => initialState;
+
+    const incrementOrReset$ = Observable.merge(         // increment or reset functions pushed through to stream
+      intervalThatStops$.mapTo(increment), 
+      reset$.mapTo(reset) 
+    );
+
+    start$
+      .switchMapTo(incrementOrReset$)
+      .startWith(initialState as any)
+      .scan((acc: number, curr: (acc: number) => number) => curr(acc))
+      .subscribe((x: number) =>
+        this.timer.nativeElement.textContent = x
+      );          
   }
-
-  onResetClicked(event) {
-    console.log('reset clicked');
-  }
-
 }
